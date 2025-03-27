356948c6a9826bfa5e9b2faa24915785f09de2d4
Kamisora <rakamiso575@gmail.com>
2025-03-27 22:13:05 +0800

    在启动类上添加@EnableScheduling注解

--- alipaySandboxBackend/src/main/java/com/alipaysandbox/alipaysandboxbackend/AlipaySandboxBackendApplication.java
+++ alipaySandboxBackend/src/main/java/com/alipaysandbox/alipaysandboxbackend/AlipaySandboxBackendApplication.java
@@ -2,8 +2,10 @@ package com.alipaysandbox.alipaysandboxbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AlipaySandboxBackendApplication {
 
     public static void main(String[] args) {
